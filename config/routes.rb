0e7947437d7f4a34d14caea372b77ca224734321
Ricardo Acevedo <ricardo.acevedodev@gmail.com>
2019-09-09 12:44:58 -0600

    testiti

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :tarifas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/home', to: 'pages#home'
+
+  get 'pages/about', to: 'pages#about'
 end
